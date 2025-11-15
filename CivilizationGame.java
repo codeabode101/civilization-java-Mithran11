dea1659d4e37e08dc0728e95dab8f32969d3b07e
Mithran11 <mithranarivuson@gmail.com>
2025-11-15 03:26:24 +0000

    CivilizationGame.java

---
+++ CivilizationGame.java
@@ -0,0 +1,12 @@
+//import java.util.Scanner();
+import java.util.*;
+import java.util.Scanner;
+
+public class ScannerExample {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input from the console
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("what is the Civilization Name: ");
+        String name = scanner.nextLine(); 
+        
